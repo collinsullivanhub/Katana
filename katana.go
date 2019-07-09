efdcceb95184ba4d76d2d2fa7678c4784ea99442
Collin Sullivan <wcsullivan@oru.edu>
2019-07-08 22:42:39 -0400

    Update katana.go

--- katana.go
+++ katana.go
@@ -29,7 +29,8 @@ var (
 	buffer       gopacket.SerializeBuffer
   	options      gopacket.SerializeOptions
 	chartslice 	 []float64
-	Rates  = []byte{0x82, 0x84, 0x8b, 0x96, 0x24, 0x30, 0x48, 0x6c, 0x03, 0x01}
+	averageSignalRate int32 = 0
+	Rates = []byte{0x82, 0x84, 0x8b, 0x96, 0x24, 0x30, 0x48, 0x6c, 0x03, 0x01}
 )
 
 var SerializationOptions = gopacket.SerializeOptions{
@@ -44,6 +45,7 @@ type Dot11ApConfig struct {
 	Encryption bool
 }
 
+
 func Serialize(layers ...gopacket.SerializableLayer) (error, []byte) {
 	buf := gopacket.NewSerializeBuffer()
 	if err2 := gopacket.SerializeLayers(buf, SerializationOptions, layers...); err2 != nil {
@@ -51,6 +53,7 @@ func Serialize(layers ...gopacket.SerializableLayer) (error, []byte) {
 	}
 	return nil, buf.Bytes()
 }
+
 
 func Dot11Info(id layers.Dot11InformationElementID, info []byte) *layers.Dot11InformationElement {
 	return &layers.Dot11InformationElement{
@@ -61,9 +64,156 @@ func Dot11Info(id layers.Dot11InformationElementID, info []byte) *layers.Dot11In
 }
 
 
+func main() {
+
+
+	rotate()
+
+	addrs, err := net.InterfaceAddrs()
+
+	if err != nil {
+		os.Stderr.WriteString("There was an error: " + err.Error() + "\n")
+		os.Exit(1)
+	}
+
+	print_interfaces := exec.Command("iwconfig | grep -w Monitor")
+	print_interfaces.Stdout = os.Stdout
+	print_interfaces.Run()
+
+	fmt.Println("__________________________________________________________________________")
+	for _, a := range addrs {
+		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To16() != nil {
+
+				os.Stdout.WriteString("\nAddress assigned to network interface: " + ipnet.IP.String() + "\n")
+			}
+		}
+	}
+
+	fmt.Println("__________________________________________________________________________")
+
+	option_select := ""
+	prompt := &survey.Select{
+		Message: "\n",
+		Options: []string{"Start Monitor", "Show beacon statistics", "Send Test Beacons", "Print RTap-Power Chart", "Observe Average Power Rates"},
+	}
+
+	fmt.Print("\n")
+	survey.AskOne(prompt, &option_select, nil)
+
+	if option_select == "Start Monitor" {
+		fmt.Println("Press key to begin...")
+		handle, err = pcap.OpenLive(device, snapshot_len, promiscuous, timeout)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer handle.Close()
+		packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+		for packet := range packetSource.Packets() {
+			display_beacons(packet)
+		}
+
+	}
+
+	if option_select == "Show beacon statistics" {
+		fmt.Println("Here are the statistics:")
+		os.Exit(3)
+	}
+
+	if option_select == "Send Test Beacons" {
+		fmt.Println("Starting...")
+		send_beacons()
+	}
+
+	if option_select == "Print RTap-Power Chart" {
+		printAChart()
+	}
+
+	if option_select == "Observe Average Power Rates" {
+		handle, err = pcap.OpenLive(device, snapshot_len, promiscuous, timeout)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+	defer handle.Close()
+	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSource.Packets() {
+			display_average_power(packet)
+		}
+	}
+
+//end main()
+}
+
+
+func display_beacons(packet gopacket.Packet) {
+
+	f, err := os.OpenFile("katana.txt", os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("failed: %s", err)
+	}
+	defer f.Close()
+
+	dot11Information := packet.Layer(layers.LayerTypeDot11)
+	//dot11elementInformation := packet.Layer(layers.LayerTypeDot11InformationElement)
+	radioInformation := packet.Layer(layers.LayerTypeRadioTap)
+
+	//DOT11 ADDR INFORMATION
+	if dot11Information != nil || radioInformation != nil {
+
+		dot11Information, _ := dot11Information.(*layers.Dot11)
+		//dot11elementInformation, _ := dot11elementInformation.(*layers.Dot11InformationElement)
+		radioInformation, _ := radioInformation.(*layers.RadioTap)
+
+		counter += 1
+
+		fmt.Print("\u001b[37mStation: \u001b[32m", dot11Information.Address3,
+			" \u001b[37mDestination: \u001b[32m", dot11Information.Address1,
+			" \u001b[37mData: \u001b[33m", dot11Information.Type,
+			" \u001b[37mSequenceNumber: \u001b[33m", dot11Information.SequenceNumber,
+			" \u001b[37mFrequency: \u001b[34m", radioInformation.ChannelFrequency,
+			" \u001b[37mDBM Antenna Signal: \u001b[34m", radioInformation.DBMAntennaSignal,
+			" \u001b[37mRate: \u001b[33m", radioInformation.Rate,
+			" \u001b[37mTX Attenuation: \u001b[35m", radioInformation.TxAttenuation,
+			" \u001b[37mDBTx Attenuation: \u001b[35m", radioInformation.DBTxAttenuation,
+			" \u001b[37mDBMTxPower: \u001b[35m", radioInformation.DBMTxPower,
+			" \u001b[37mAntenna: \u001b[35m", radioInformation.Antenna, " \u001b[31m Beacons Captured: ", counter) 
+			//"ESSID: ", dot11elementInformation.Info)
+
+		if dot11Information.Address3 != nil {
+			fmt.Fprintln(f, dot11Information.Address3)
+		}
+
+		fmt.Println("")
+	}
+
+}
+
+func display_average_power(packet gopacket.Packet) {
+	radioInformation := packet.Layer(layers.LayerTypeRadioTap)
+	if radioInformation != nil {
+		radioInformation, _ := radioInformation.(*layers.RadioTap)
+		fmt.Print("\u001b[37mAntenna Signal: \u001b[34m", radioInformation.DBMAntennaSignal, "\n")
+	}
+
+}
+
+
+func printAChart() {
+
+	//Develop
+	graph := asciigraph.Plot(chartslice)
+	fmt.Println(graph)
+}
+
 
 func send_beacons() {
 
+	//Need high TX card such as Atheros or TP-Link
     handle, err = pcap.OpenLive(device, snapshot_len, promiscuous, timeout)
     if err != nil {log.Fatal(err)}
     defer handle.Close()
@@ -104,127 +254,6 @@ func send_beacons() {
   }
 }
 
-func main() {
-
-
-	rotate()
-
-	addrs, err := net.InterfaceAddrs()
-
-	if err != nil {
-		os.Stderr.WriteString("There was an error: " + err.Error() + "\n")
-		os.Exit(1)
-	}
-
-	print_interfaces := exec.Command("iwconfig | grep -w Monitor")
-	print_interfaces.Stdout = os.Stdout
-	print_interfaces.Run()
-
-	fmt.Println("__________________________________________________________________________")
-	for _, a := range addrs {
-		if ipnet, ok := a.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
-			if ipnet.IP.To16() != nil {
-
-				os.Stdout.WriteString("\nAddress assigned to network interface: " + ipnet.IP.String() + "\n")
-			}
-		}
-	}
-
-	fmt.Println("__________________________________________________________________________")
-
-	option_select := ""
-	prompt := &survey.Select{
-		Message: "\n",
-		Options: []string{"Start Monitor", "Show beacon statistics", "Send Test Beacons", "Print RTap-Power Chart"},
-	}
-
-	fmt.Print("\n")
-	survey.AskOne(prompt, &option_select, nil)
-
-	if option_select == "Start Monitor" {
-		fmt.Println("Press key to begin...")
-	}
-
-	if option_select == "Show beacon statistics" {
-		fmt.Println("Here are the statistics:")
-		os.Exit(3)
-	}
-
-	if option_select == "Send Test Beacons" {
-		fmt.Println("Starting...")
-		send_beacons()
-		os.Exit(3)
-	}
-
-	if option_select == "Print RTap-Power Chart" {
-		printAChart()
-		os.Exit(3)
-	}
-
-	handle, err = pcap.OpenLive(device, snapshot_len, promiscuous, timeout)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer handle.Close()
-	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-
-	//  print_artwork()
-
-	for packet := range packetSource.Packets() {
-		display_beacons(packet)
-	}
-
-}
-
-func display_beacons(packet gopacket.Packet) {
-
-	f, err := os.OpenFile("katana.txt", os.O_APPEND|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatalf("failed: %s", err)
-	}
-	defer f.Close()
-
-	dot11Information := packet.Layer(layers.LayerTypeDot11)
-	//dot11elementInformation := packet.Layer(layers.LayerTypeDot11InformationElement)
-	radioInformation := packet.Layer(layers.LayerTypeRadioTap)
-
-	//DOT11 ADDR INFORMATION
-	if dot11Information != nil || radioInformation != nil {
-
-		dot11Information, _ := dot11Information.(*layers.Dot11)
-		//dot11elementInformation, _ := dot11elementInformation.(*layers.Dot11InformationElement)
-		radioInformation, _ := radioInformation.(*layers.RadioTap)
-
-		counter += 1
-
-		fmt.Print("\u001b[37mStation: \u001b[32m", dot11Information.Address3,
-			" \u001b[37mDestination: \u001b[32m", dot11Information.Address1,
-			" \u001b[37mData: \u001b[33m", dot11Information.Type,
-			" \u001b[37mSequenceNumber: \u001b[33m", dot11Information.SequenceNumber,
-			" \u001b[37mFrequency: \u001b[34m", radioInformation.ChannelFrequency,
-			" \u001b[37mDBM AS: \u001b[34m", radioInformation.DBMAntennaSignal,
-			" \u001b[37mRate: \u001b[33m", radioInformation.Rate,
-			" \u001b[37mTX Attenuation: \u001b[35m", radioInformation.TxAttenuation,
-			" \u001b[37mDBTx Attenuation: \u001b[35m", radioInformation.DBTxAttenuation,
-			" \u001b[37mDBMTxPower: \u001b[35m", radioInformation.DBMTxPower,
-			" \u001b[37mAntenna: \u001b[35m", radioInformation.Antenna, " \u001b[31m Beacons Captured: ", counter)
-
-		if dot11Information.Address3 != nil {
-			fmt.Fprintln(f, dot11Information.Address3)
-		}
-
-		fmt.Println("")
-	}
-
-}
-
-func printAChart() {
-
-	graph := asciigraph.Plot(chartslice)
-	fmt.Println(graph)
-}
 
 func printA() {
 
@@ -234,12 +263,14 @@ func printA() {
 	print_artwork()
 }
 
+
 func printB() {
 	clearscreen := exec.Command("clear")
 	clearscreen.Stdout = os.Stdout
 	clearscreen.Run()
 	print_artwork2()
 }
+
 
 func printC() {
 	clearscreen := exec.Command("clear")
@@ -248,6 +279,7 @@ func printC() {
 	print_artwork3()
 
 }
+
 
 func rotate() {
 	for {
@@ -309,8 +341,11 @@ func rotate() {
 	}
 }
 
+
 func print_artwork() {
 	color.Blue(`
+
+
                                           KATANA
 
                                       ://oyydmmho++/--.
@@ -381,6 +416,8 @@ func print_artwork() {
 
 func print_artwork2() {
 	color.Green(`
+
+
                                           KATANA
 
                                       ://oyydmmho++/--.
